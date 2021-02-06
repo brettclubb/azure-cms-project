1e73b00fff7e8e446766bf055235336c88b60183
brettclubb <brett.clubb@gmail.com>
2021-02-06 12:57:48 -0600

    Adjusts logging level again - crashing app

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,10 +10,10 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.logger.setLevel(logging.INFO)
+app.logger.setLevel(logging.WARNING)
 streamHandler = logging.StreamHandler()
-streamHandler.setLevel(logging.INFO)
-app.logger.addHandler(streamHandler)
+streamHandler.setLevel(logging.WARNING)
+app.logger.addhandler(streamHandler)
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
